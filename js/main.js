4dffeb820bf715e62e61bbd9e8ab54af3e12ecef
dough <viktor.melin@me.com>
2022-11-10 16:59:49 +0100

    :sparkles: added menu toggle from script

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 class Donut {
-    constructor(name, price, categories, imgPath) {
-        this.name = name,
-        this.price = price,
-        this.categories = categories,
-        this.imgPath = imgPath
-    }
+	constructor(name, price, categories, imgPath) {
+		(this.name = name),
+			(this.price = price),
+			(this.categories = categories),
+			(this.imgPath = imgPath);
+	}
 }
 
 /* Kategorier: 
@@ -22,76 +22,93 @@ strawberry
 // Bilder tagna från https://github.com/aaronfrost/DonutsApi/tree/main/static/images
 
 const donutsArray = [
-    new Donut(
-        'Chokladmunk med vaniljfyllning',
-        23,
-        ['chocolate', 'filled'],
-        ['img/chocolate-iced-custard-filled-banner.jpeg', 'img/chocolate-iced-custard-filled.jpeg']
-    ),
-    new Donut(
-        'Chokladmunk',
-        18,
-        ['chocolate'],
-        ['img/chocolate-iced-glazed-banner.jpeg', 'img/chocolate-iced-glazed.jpeg']
-    ),
-    new Donut(
-        'Kanelpudrad munk med äppelfyllning',
-        25,
-        ['filled', 'apple', 'cinnamon', 'powdered'],
-        ['img/cinnamon-apple-filled-banner.jpeg', 'img/cinnamon-apple-filled.jpeg']
-    ),
-    new Donut(
-        'Glaserad kanelmunk',
-        13,
-        ['glazed', 'cinnamon'],
-        ['img/glazed-cinnamon-banner.jpeg', 'img/glazed-cinnamon.jpeg']
-    ),
-    new Donut(
-        'Glaserad munk med citronfyllning',
-        23,
-        ['glazed', 'lemon'],
-        ['img/glazed-lemon-filled-banner.jpeg', 'img/glazed-lemon-filled.jpeg']
-    ),
-    new Donut(
-        'Munk med chokladfyllning',
-        16,
-        ['chocolate', 'filled'],
-        ['img/original-filled-chocolate-kreme™-banner.jpeg', 'img/original-filled-chocolate-kreme™.jpeg']
-    ),
-    new Donut(
-        'Glaserad munk',
-        8,
-        ['glazed'],
-        ['img/original-glazed-doughnut-banner.jpeg', 'img/original-glazed-doughnut.jpeg']
-    ),
-    new Donut(
-        'Pudrad munk med blåbärsfyllning',
-        25,
-        ['powdered', 'blueberry'],
-        ['img/powdered-blueberry-filled-banner.jpeg', 'img/powdered-blueberry-filled.jpeg']
-    ),
-    new Donut(
-        'Pudrad munk med jordgubbsfyllning',
-        25,
-        ['powdered', 'strawberry'],
-        ['img/powdered-strawberry-filled-banner.jpeg', 'img/powdered-strawberry-filled.jpeg']
-    ),
-    new Donut(
-        'Jordgubbsmunk',
-        11,
-        ['strawberry'],
-        ['img/strawberry-iced-banner.jpeg', 'img/strawberry-iced.jpeg']
-    )
+	new Donut(
+		'Chokladmunk med vaniljfyllning',
+		23,
+		['chocolate', 'filled'],
+		[
+			'img/chocolate-iced-custard-filled-banner.jpeg',
+			'img/chocolate-iced-custard-filled.jpeg',
+		]
+	),
+	new Donut(
+		'Chokladmunk',
+		18,
+		['chocolate'],
+		['img/chocolate-iced-glazed-banner.jpeg', 'img/chocolate-iced-glazed.jpeg']
+	),
+	new Donut(
+		'Kanelpudrad munk med äppelfyllning',
+		25,
+		['filled', 'apple', 'cinnamon', 'powdered'],
+		['img/cinnamon-apple-filled-banner.jpeg', 'img/cinnamon-apple-filled.jpeg']
+	),
+	new Donut(
+		'Glaserad kanelmunk',
+		13,
+		['glazed', 'cinnamon'],
+		['img/glazed-cinnamon-banner.jpeg', 'img/glazed-cinnamon.jpeg']
+	),
+	new Donut(
+		'Glaserad munk med citronfyllning',
+		23,
+		['glazed', 'lemon'],
+		['img/glazed-lemon-filled-banner.jpeg', 'img/glazed-lemon-filled.jpeg']
+	),
+	new Donut(
+		'Munk med chokladfyllning',
+		16,
+		['chocolate', 'filled'],
+		[
+			'img/original-filled-chocolate-kreme™-banner.jpeg',
+			'img/original-filled-chocolate-kreme™.jpeg',
+		]
+	),
+	new Donut(
+		'Glaserad munk',
+		8,
+		['glazed'],
+		[
+			'img/original-glazed-doughnut-banner.jpeg',
+			'img/original-glazed-doughnut.jpeg',
+		]
+	),
+	new Donut(
+		'Pudrad munk med blåbärsfyllning',
+		25,
+		['powdered', 'blueberry'],
+		[
+			'img/powdered-blueberry-filled-banner.jpeg',
+			'img/powdered-blueberry-filled.jpeg',
+		]
+	),
+	new Donut(
+		'Pudrad munk med jordgubbsfyllning',
+		25,
+		['powdered', 'strawberry'],
+		[
+			'img/powdered-strawberry-filled-banner.jpeg',
+			'img/powdered-strawberry-filled.jpeg',
+		]
+	),
+	new Donut(
+		'Jordgubbsmunk',
+		11,
+		['strawberry'],
+		['img/strawberry-iced-banner.jpeg', 'img/strawberry-iced.jpeg']
+	),
 ];
-
-
 
 /**
  *  Print donutsArray-items as an Article and add to .donut-Section
  */
- var donutArticle ="", i;
- for (let i = 0; i < donutsArray.length; i++) { //loop through the whole array.
-    donutArticle= donutArticle + `    
+var donutArticle = '',
+	i;
+for (let i = 0; i < donutsArray.length; i++) {
+	//loop through the whole array.
+	donutArticle =
+		donutArticle +
+		/*html*/ `    
     <article class="donut__item">
         <h2>${donutsArray[i].name}</h2> 
         <div>
@@ -110,56 +127,81 @@ const donutsArray = [
         <div class="rating">
             <!-- Rating med stjärnor -->
         </div>
-    </article>` + i; // For development, remove "+ i;" to hide item array index on weppage.
+    </article>` +
+		i; // For development, remove "+ i;" to hide item array index on weppage.
 
-
-    console.log(donutsArray[i]); //For development
- }
- document.querySelector(".donut-section").innerHTML = donutArticle;
+	console.log(donutsArray[i]); //For development
+}
+document.querySelector('.donut-section').innerHTML = donutArticle;
 
 /******************************************************************
  *  Item Buttons (Ej klar, behöver göras om?)
  *****************************************************************/
-const decrementItem = document.querySelectorAll(".decrement-item");
-const changeValueOfItem = document.querySelector(".number-of-items");
-const incrementItem = document.querySelectorAll(".increment-item");
-
+const decrementItem = document.querySelectorAll('.decrement-item');
+const changeValueOfItem = document.querySelector('.number-of-items');
+const incrementItem = document.querySelectorAll('.increment-item');
 
 /**
  *  Increment item function
  */
 
-const increaseValueOfItem = function() {
-    if (changeValueOfItem.value != 99) { // To have some control over the order.
-        changeValueOfItem.value++;
-        console.log(changeValueOfItem.value) //For development
-    }
-}
+const increaseValueOfItem = function () {
+	if (changeValueOfItem.value != 99) {
+		// To have some control over the order.
+		changeValueOfItem.value++;
+		console.log(changeValueOfItem.value); //For development
+	}
+};
 //Add eventlistener increaseValueOfItem to all increment buttons
 for (let i = 0; i < incrementItem.length; i++) {
-    incrementItem[i].addEventListener("click",increaseValueOfItem);
+	incrementItem[i].addEventListener('click', increaseValueOfItem);
 }
 
 /**
  *  Decrement item function
  */
- const decreaseValueOfItem = function() {
-    if (changeValueOfItem.value != 0) { // So it's impossible to add negatime number of items to basket
-        changeValueOfItem.value --;
-        console.log(changeValueOfItem.value) //For development
-    } 
-}
+const decreaseValueOfItem = function () {
+	if (changeValueOfItem.value != 0) {
+		// So it's impossible to add negatime number of items to basket
+		changeValueOfItem.value--;
+		console.log(changeValueOfItem.value); //For development
+	}
+};
 //Add eventlistener decreaseValueOfItem to all decrement buttons
 for (let i = 0; i < decrementItem.length; i++) {
-    decrementItem[i].addEventListener("click",decreaseValueOfItem);
+	decrementItem[i].addEventListener('click', decreaseValueOfItem);
 }
 
 /**
  *  Add to basket function
  */
 
-
-
- /**
+/**
  *  Array sorting (add these functions to an eventlistener)
  */
+
+const filterButton = document.querySelector('.navbar__menu > button');
+const filterElement = document.querySelector('#filterMenu');
+let filterMenuVisible = false;
+
+filterButton.addEventListener('click', () => {
+	filterButton.setAttribute('aria-expanded', !filterMenuVisible);
+	filterMenuVisible = JSON.parse(filterButton.getAttribute('aria-expanded'));
+
+	if (filterMenuVisible) {
+		filterElement.style.display = 'flex';
+		window.setTimeout(() => {
+			filterElement.style.opacity = 1;
+			filterElement.style.transform = 'scale(1)';
+		}, 0);
+	} else {
+		filterElement.style.opacity = 0;
+		filterElement.style.transform = 'scale(0)';
+
+		window.setTimeout(() => {
+			filterElement.style.display = 'none';
+		}, 200);
+	}
+
+	if (filterMenuVisible) filterElement.querySelector('select').focus();
+});
