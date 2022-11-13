2e04293f4237b0b2051ea2f298cf114a24f15796
dough <viktor.melin@me.com>
2022-11-13 22:42:55 +0100

    :sparkles: start rating for donuts; class changes

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ const donutsArray = [
 			'img/chocolate-iced-custard-filled-banner.jpeg',
 			'img/chocolate-iced-custard-filled.jpeg',
 		],
-		rating: 5
+		rating: 5,
 	},
 	{
 		id: 2,
@@ -33,7 +33,7 @@ const donutsArray = [
 			'img/chocolate-iced-glazed-banner.jpeg',
 			'img/chocolate-iced-glazed.jpeg',
 		],
-		rating: 4.5
+		rating: 4.5,
 	},
 	{
 		id: 3,
@@ -44,7 +44,7 @@ const donutsArray = [
 			'img/cinnamon-apple-filled-banner.jpeg',
 			'img/cinnamon-apple-filled.jpeg',
 		],
-		rating: 4.5
+		rating: 4.5,
 	},
 	{
 		id: 4,
@@ -52,7 +52,7 @@ const donutsArray = [
 		price: 13,
 		categories: ['glazed', 'cinnamon'],
 		images: ['img/glazed-cinnamon-banner.jpeg', 'img/glazed-cinnamon.jpeg'],
-		rating: 5
+		rating: 5,
 	},
 	{
 		id: 5,
@@ -63,7 +63,7 @@ const donutsArray = [
 			'img/glazed-lemon-filled-banner.jpeg',
 			'img/glazed-lemon-filled.jpeg',
 		],
-		rating: 4
+		rating: 4,
 	},
 	{
 		id: 6,
@@ -74,7 +74,7 @@ const donutsArray = [
 			'img/original-filled-chocolate-kreme™-banner.jpeg',
 			'img/original-filled-chocolate-kreme™.jpeg',
 		],
-		rating: 4
+		rating: 4,
 	},
 	{
 		id: 7,
@@ -85,7 +85,7 @@ const donutsArray = [
 			'img/original-glazed-doughnut-banner.jpeg',
 			'img/original-glazed-doughnut.jpeg',
 		],
-		rating: 5
+		rating: 5,
 	},
 	{
 		id: 8,
@@ -96,7 +96,7 @@ const donutsArray = [
 			'img/powdered-blueberry-filled-banner.jpeg',
 			'img/powdered-blueberry-filled.jpeg',
 		],
-		rating: 4.5
+		rating: 4.5,
 	},
 	{
 		id: 9,
@@ -107,7 +107,7 @@ const donutsArray = [
 			'img/powdered-strawberry-filled-banner.jpeg',
 			'img/powdered-strawberry-filled.jpeg',
 		],
-		rating: 4
+		rating: 4,
 	},
 	{
 		id: 10,
@@ -115,7 +115,7 @@ const donutsArray = [
 		price: 11,
 		categories: ['strawberry'],
 		images: ['img/strawberry-iced-banner.jpeg', 'img/strawberry-iced.jpeg'],
-		rating: 5
+		rating: 5,
 	},
 ];
 
@@ -124,14 +124,14 @@ const donutsArray = [
  **********************************************************/
 const cartItems = [];
 
-const donutListEl = document.querySelector('.donut-section');
+const donutListEl = document.querySelector('.donuts');
 const donutIncreaseCount = (id) => {
 	const donutEl = document.querySelector(`[data-id="${id}"]`);
-	const currentCountEl = donutEl.querySelector('.quantity input');
-	const currentCount = Number(currentCountEl.value) + 1; 
+	const currentCountEl = donutEl.querySelector('.donuts__item_quantity input');
+	const currentCount = Number(currentCountEl.value) + 1;
 	if (currentCountEl.value >= 99) {
-        currentCountEl.value = currentCountEl.value.slice(0,2); //remove 3rd digit
-    } else {
+		currentCountEl.value = currentCountEl.value.slice(0, 2); //remove 3rd digit
+	} else {
 		currentCountEl.value++;
 	}
 	console.log(Number(currentCountEl.value));
@@ -139,18 +139,19 @@ const donutIncreaseCount = (id) => {
 };
 const donutDecreaseCount = (id) => {
 	const donutEl = document.querySelector(`[data-id="${id}"]`);
-	const currentCountEl = donutEl.querySelector('.quantity input');
+	const currentCountEl = donutEl.querySelector('.donuts__item_quantity input');
 	const currentCount = Number(currentCountEl.value) - 1;
 	if (currentCountEl.value != 0) {
-        currentCountEl.value--; 
-    }
+		currentCountEl.value--;
+	}
 	console.log(Number(currentCountEl.value));
 	console.log(currentCount);
 };
 
 const donutAddToCart = (id) => {
 	const currentCount = Number(
-		document.querySelector(`[data-id="${id}"] .quantity input`).value
+		document.querySelector(`[data-id="${id}"] .donuts__item_quantity input`)
+			.value
 	);
 
 	if (currentCount > 0) {
@@ -184,6 +185,25 @@ const updateCartDOM = () => {
 	console.log(cartItems);
 };
 
+const generateStarRating = (rating) => {
+	const starIcon = '<i class="fa-solid fa-star"></i>';
+	const starHalfIcon = '<i class="fa-solid fa-star-half"></i>';
+
+	const splitPoints = rating.toString().split('.');
+	const fullPoints = splitPoints[0];
+	const halfPoint = splitPoints.length > 1 ? splitPoints[1] : 0;
+
+	let ratingEl = '';
+
+	for (let i = 0; i < Number(fullPoints); i++) {
+		ratingEl += starIcon;
+	}
+
+	if (halfPoint) ratingEl += starHalfIcon;
+
+	return ratingEl;
+};
+
 /*********************************************************
  * Create HTML
  **********************************************************/
@@ -192,26 +212,32 @@ const generateDonuts = () => {
 	let donuts = [];
 	for (const donut of donutsArray) {
 		donuts += /*html*/ `
-      <article class="donut__item" data-id=${donut.id}>
+      <article class="donuts__item" data-id=${donut.id}>
         <h2>${donut.name}</h2> 
-        <div>
+        <div class="donuts__item_image">
           <img
             src="${donut.images[1]}" 
             alt="A picture of a donut"
           />
         </div>
-        <p>${donut.price} <span>kr</span></p>
-        <div class="quantity">
-          <button onclick="donutDecreaseCount(${donut.id})">-</button>
+				<div class="donuts__item_info">
+					<p>${donut.price} kr</p>
+					<p>${generateStarRating(donut.rating)}</p>
+				</div>
+        
+        <div class="donuts__item_quantity">
+          <button class="button button--background" onclick="donutDecreaseCount(${
+						donut.id
+					})"><i class="fa-solid fa-minus"></i></button>
           <input type="number" value="0"  min="0" max="99" oninput="this.value = !!this.value && Math.abs(this.value) 
 		  >= 0 ? Math.abs(this.value) : null"/> <!--No negative number or letters allowed-->
-          <button onclick="donutIncreaseCount(${donut.id})">+</button>
+          <button class="button button--background" onclick="donutIncreaseCount(${
+						donut.id
+					})"><i class="fa-solid fa-plus"></i></button>
         </div>
-        <button onclick="donutAddToCart(${donut.id})">Lägg till</button>
-        <div class="rating">
-			<p>Betyg : ${donut.rating}</p>
-            <!-- Rating med stjärnor -->
-        </div>
+        <button class="donuts__item_addcart button button--background" onclick="donutAddToCart(${
+					donut.id
+				})">Lägg till</button>
       </article>
     `;
 	}
@@ -219,8 +245,6 @@ const generateDonuts = () => {
 };
 
 generateDonuts();
-
-  
 
 /*********************************************************
  * Input field validation
